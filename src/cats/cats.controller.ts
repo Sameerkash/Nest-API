98fcfe9208c6788ca3837e00c8a999140ee3c707
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-10-24 23:11:21 +0530

    till guards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -5,11 +5,15 @@ import {
   ForbiddenException,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
   UseFilters,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filters';
+import { JoiValidationPipe } from 'src/common/pipes/validation.pipe';
 import { CatsService } from './cats.service';
 import { CreateCatDto, UpdateCatDto } from './create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
@@ -24,16 +28,15 @@ export class CatsController {
   }
 
   @Get()
-  @UseFilters(new HttpExceptionFilter())
+  @UsePipes(new ValidationPipe())
   async findAll(): Promise<Cat[]> {
     throw new ForbiddenException();
   }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
+  async findOne(@Param('id', new ParseIntPipe()) id) {
+    return this.catsService.findOne(id);
   }
-
+  
   @Put(':id')
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
--- src/cats/create-cat.dto.ts
+++ src/cats/create-cat.dto.ts
@@ -1,6 +1,13 @@
+import { IsString, IsInt } from 'class-validator';
+
 export class CreateCatDto {
+  @IsString()
   name: string;
+
+  @IsInt()
   age: number;
+
+  @IsString()
   breed: string;
 }
 
